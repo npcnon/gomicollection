0d6a9bae9a2c3d884dd5d3a391d0007d00623cb9
npcnon <afkmhafric@gmail.com>
2025-01-10 03:31:28 +0800

    init 🎋

---
+++ src/main/java/com/company/gomicollection/entity/Employee.java
@@ -0,0 +1,105 @@
+package com.company.gomicollection.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "EMPLOYEE")
+@Entity
+public class Employee {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+    @NotNull
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String first_name;
+    @Column(name = "MIDDLE_NAME")
+    private String middle_name;
+    @Column(name = "LAST_NAME", nullable = false)
+    @NotNull
+    private String last_name;
+    @Column(name = "AGE", nullable = false)
+    @NotNull
+    private Integer age;
+    @Column(name = "ADDRESS", nullable = false)
+    @NotNull
+    private String address;
+    @Column(name = "CONTACT_NUMBER", nullable = false)
+    @NotNull
+    private String contact_number;
+    @Column(name = "EMAIL", nullable = false)
+    @NotNull
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    public void setMiddle_name(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
